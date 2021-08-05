792ff7008dde9480345449df92b0518180923816
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-08-05 10:57:38 +0800

    second commit

---
+++ T11ADProject/src/main/java/sg/edu/iss/ad/T11AdProjectApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T11AdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T11AdProjectApplication.class, args);
+	}
+
+}
---
+++ T11ADProject/src/test/java/sg/edu/iss/ad/T11AdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T11AdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
