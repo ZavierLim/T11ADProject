f1616a7229f7d4d5886cea9aa3e2d0dcd6345a37
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-08-06 11:23:58 +0800

    added crossorigin for react frontend

--- T11ADProject/src/main/java/sg/edu/iss/ad/controller/UserController.java
+++ T11ADProject/src/main/java/sg/edu/iss/ad/controller/UserController.java
@@ -3,6 +3,7 @@ package sg.edu.iss.ad.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import sg.edu.iss.ad.model.User;
 import sg.edu.iss.ad.service.UserService;
 
+@CrossOrigin(origins= "http://localhost:3000")
 @RestController
 @RequestMapping("")
 public class UserController {
