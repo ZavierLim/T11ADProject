3f7d91c43f868fcd5f60153bae30c88ed1232ea9
HarrisonTheG <harrison28590@gmail.com>
2021-08-09 15:55:14 +0800

    Modify stockcandle API

--- T11ADProject/src/main/java/sg/edu/iss/ad/controller/CandleController.java
+++ T11ADProject/src/main/java/sg/edu/iss/ad/controller/CandleController.java
@@ -1,20 +1,24 @@
 package sg.edu.iss.ad.controller;
 
-import com.alibaba.fastjson.JSON;
+import java.util.List;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSON;
+
 import sg.edu.iss.ad.model.CandleModel;
 import sg.edu.iss.ad.utility.candleDataConvertor;
 
-import java.util.Date;
-import java.util.List;
-
+@CrossOrigin
 @RestController
 public class CandleController {
     @GetMapping("hello")
@@ -22,9 +26,9 @@ public class CandleController {
         return "hello";
     }
 
-    @GetMapping("/getCandleData")
-    public String getCandleData(){
-        String url="https://yfapi.net/v8/finance/chart/AAPL?range=1d&region=US&interval=1m";
+    @GetMapping("/getCandleData/{ticker}")
+    public String getCandleData(@PathVariable("ticker") String ticker){
+        String url="https://yfapi.net/v8/finance/chart/" + ticker + "?range=200d&region=US&interval=1d";
         RestTemplate restTemplate =new RestTemplate();
         HttpHeaders headers=new HttpHeaders();
         headers.add("Accept","application/json");
@@ -32,9 +36,7 @@ public class CandleController {
         HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(null,headers);
         ResponseEntity<String> rawResult = restTemplate.exchange(url, HttpMethod.GET,httpEntity,String.class);
 
-
         List<CandleModel> result = candleDataConvertor.candleResultToList(rawResult.getBody());
-
 
         System.out.println(result.size());
 
