482e9abe7e62fb045914afca1bb1979c707f0093
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-08-10 22:17:03 +0800

    completed user API

--- T11ADProject/src/main/java/sg/edu/iss/ad/controller/UserController.java
+++ T11ADProject/src/main/java/sg/edu/iss/ad/controller/UserController.java
@@ -1,12 +1,17 @@
 package sg.edu.iss.ad.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,9 +49,18 @@ public class UserController {
 	}
 	
 	//update user API
-	@PostMapping("/users/update")
-	public User updateUser(@RequestBody User user) {
+	@PutMapping("/users/update/{id}")
+	public User updateUser(@PathVariable String id,@RequestBody User user) {
 		return Uservice.updateUser(user);
+	}
+	
+	@DeleteMapping("/users/delete/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteUser(@PathVariable Long id){
+		User user= Uservice.FindUser(id);
+		Uservice.deleteUser(user);
+		Map<String,Boolean> response=new HashMap<>();
+		response.put("deleted",Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 	
 	
