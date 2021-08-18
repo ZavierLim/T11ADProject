ddfb718f74fda724ec68e37a67e62a561b15b954
HarrisonTheG <harrison28590@gmail.com>
2021-08-18 13:16:56 +0800

    d

--- T11ADProject/src/main/java/sg/edu/iss/ad/repository/UserCandleWatchListRepository.java
+++ T11ADProject/src/main/java/sg/edu/iss/ad/repository/UserCandleWatchListRepository.java
@@ -2,7 +2,10 @@ package sg.edu.iss.ad.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,7 +16,8 @@ public interface UserCandleWatchListRepository extends JpaRepository<UserCandleW
 	public List<UserCandleWatchList> findwatchlistbyusernameandticker(@Param("username") String username,
 																	  @Param("stockticker") String stockticker);
 
-
-	@Query("delete from UserCandleWatchList ucwl where ucwl.UserStockWatchList.Stock.StockTicker=:stockTicker")
+	@Transactional
+	 @Modifying
+	@Query("delete from UserCandleWatchList ucwl where ucwl.UserStockWatchList.Stock.StockTicker =: stockTicker")
 	public void deletewatchlistbystockticker(@Param("stockTicker") String stockTicker);
 }
--- T11ADProject/src/main/java/sg/edu/iss/ad/service/CandleService.java
+++ T11ADProject/src/main/java/sg/edu/iss/ad/service/CandleService.java
@@ -27,8 +27,8 @@ public class CandleService{
         RestTemplate restTemplate =new RestTemplate();
         HttpHeaders headers=new HttpHeaders();
         headers.add("Accept","application/json");
-        headers.add("x-api-key","eg3Z4ml4ik5Grz5tGNMlc7qsZz18VnEo21ERKTYp");
-        //headers.add("x-api-key","VTr2Z2gNmk7rVPuHnVMnyWw6tfGcEsbaHFWUixU7");
+        //headers.add("x-api-key","eg3Z4ml4ik5Grz5tGNMlc7qsZz18VnEo21ERKTYp");
+        headers.add("x-api-key","VTr2Z2gNmk7rVPuHnVMnyWw6tfGcEsbaHFWUixU7");
         HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(null,headers);
         ResponseEntity<String> rawResult = restTemplate.exchange(url, HttpMethod.GET,httpEntity,String.class);
 
